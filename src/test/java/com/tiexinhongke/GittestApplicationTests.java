ac91a865ed234f06cba7430befbc80a4a125e400
liuyalu <deshan19882020@163.com>
2020-03-28 09:35:34 +0800

    git测试第一次提交

---
+++ src/test/java/com/tiexinhongke/GittestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiexinhongke;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GittestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ src/test/java/com/tiexinhongke/test1.java
@@ -0,0 +1,7 @@
+package com.tiexinhongke;
+
+public class test1 {
+    Integer a = 1;
+    Integer b = 2;
+    Integer c = 3;
+}
